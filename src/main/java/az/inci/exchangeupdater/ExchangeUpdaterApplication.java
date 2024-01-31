155ee006ccd3bf53c9e0297860c319e897c6f2aa
Museyib <museyib.e@gmail.com>
2024-01-31 09:37:48 +0400

    Execute at startup

--- src/main/java/az/inci/exchangeupdater/ExchangeUpdaterApplication.java
+++ src/main/java/az/inci/exchangeupdater/ExchangeUpdaterApplication.java
@@ -1,15 +1,27 @@
 package az.inci.exchangeupdater;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@RequiredArgsConstructor
 public class ExchangeUpdaterApplication
 {
+    private final WebDataService webDataService;
+    private final DataUpdateService dataUpdateService;
     public static void main(String[] args)
     {
         SpringApplication.run(ExchangeUpdaterApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner executeAtStartup()
+    {
+        return args -> dataUpdateService.updateExchange(webDataService.getExchangeList());
     }
 }
